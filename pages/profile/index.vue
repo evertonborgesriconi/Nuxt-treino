6c55c46773c7ba0cd4d85f4e13c73a32d39057b4
Everton Riconi <evertonriconi@gmail.com>
2023-09-04 15:44:56 -0300

    alt

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,4 +1,39 @@
+<script setup lang="ts">
 
+
+interface Poke {
+  name: string;
+  type: string;
+  evolution: number;
+}
+
+const counts = ref([10, 20, 50])
+//const double = computed(() => count.value * 2)
+console.log(counts.value[2]);
+const valor = ref(0);
+const increment = () => valor.value++;
+
+const pokemons : Poke = {
+    name : "Bulba",
+    type : "Grenn",
+    evolution: 12
+}
+
+console.log(pokemons);
+
+
+
+</script>
 <template>
-    <div>Profile</div>
+    <div v-for="count in counts">
+        <div>Profile: {{ count }}</div>
+    </div>
+
+    <button @click="increment"> Mais + </button>
+    <div>{{ valor }}</div>
+
 </template>
+
+<style>
+@import url("./style.css");
+</style>
